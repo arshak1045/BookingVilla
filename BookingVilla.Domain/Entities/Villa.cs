f47531c8d403052b5a721c9c8f75ea055c671f3d
arshak1045 <hayryan1045@gmail.com>
2023-12-10 17:17:13 +0400

    Initial commit.

---
+++ BookingVilla.Domain/Entities/Villa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingVilla.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [MinLength(5)]
+        public required string Name { get; set; }
+        [MaxLength(300)]
+        public string? Description { get; set; }
+        [Range(1,10000)]
+        public double Price { get; set; }
+        [Display(Name = "Square Feet")]
+        [Range(1,2000)]
+        public int Sqft { get; set; }
+        [Range(1, 10)]
+        public int Occupancy { get; set; }
+        [Display(Name="Image URL")]
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ BookingVilla/Controllers/VillaController.cs
@@ -0,0 +1,45 @@
+﻿using BookingVilla.Domain.Entities;
+using BookingVilla.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingVilla.Controllers
+{
+    public class VillaController : Controller
+    {
+
+        private readonly ApplicationDbContext _DbContext;
+
+        public VillaController(ApplicationDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _DbContext.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if(villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("", "The name and description should be differ");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _DbContext.Villas.Add(villa);
+                _DbContext.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+    }
+}
